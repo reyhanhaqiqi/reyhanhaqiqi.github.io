50793ef524ce44b79caaaac5d17fdb8a2df2e78f
reyhanhaqiqi <saifunaris@gmail.com>
2022-04-15 13:31:35 +0700

    first commit

---
+++ JS/main.js
@@ -0,0 +1,46 @@
+/* Preloader Start */
+function preloader() {
+    $(() => {
+
+        setInterval(() => {
+
+            let p = $('.preloader');
+            p.css('opacity', 0);
+
+            setInterval(
+                () => p.remove(),
+                parseInt(p.css('--duration')) * 1000
+            );
+
+        }, 1000)
+
+    });
+}
+setInterval(() => preloader(), 2000);
+/* PreLoader End */
+
+
+/* Typed-Text Start*/
+(function ($) {
+    "use strict"
+
+    /* Typed Initiate */
+    if ($('.typed-text-output').length == 1) {
+        var typed_strings = $('.typed-text').text();
+        var typed = new Typed('.typed-text-output', {
+            strings: typed_strings.split(', '),
+            typeSpeed: 100,
+            backSpeed: 20,
+            smartBackspace: false,
+            loop: true
+        });
+    }
+})(jQuery);
+/* Typed-Text End */
+
+
+/* AOS Start */
+AOS.init({
+    once: true,
+});
+/* AOS End */
